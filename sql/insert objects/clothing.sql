0c476ce1ae3df28c7aa4d086b0bc5b9b611a6bc7
V0idKernel621 <m.rezaelahi90@gmail.com>
2026-09-09 16:52:57 +0330

    Create clothing.sql

---
+++ sql/insert objects/clothing.sql
@@ -0,0 +1,17 @@
+INSERT INTO object (id,type_id,name)
+VALUES (1,2,'Red T-Shirt');
+
+INSERT INTO object_value (object_id, attribute_id, text_value)
+VALUES (1, 1, 'Red');
+
+INSERT INTO object_value (object_id, attribute_id, text_value)
+VALUES (1, 2, 'L');
+
+INSERT INTO object_value (object_id, attribute_id, text_value)
+VALUES (1, 3, 'Any');
+
+INSERT INTO object_value (object_id, attribute_id, text_value)
+VALUES (1, 4, 'Shirt');
+
+INSERT INTO object_value (object_id, attribute_id, text_value)
+VALUES (1, 5, 'Cotton');
