0e67a32edccecec90c5eb5439cb34a06998b4b0d
V0idKernel621 <m.rezaelahi90@gmail.com>
2026-09-09 16:51:36 +0330

    Create clothing.sql

---
+++ sql/insert allowed values/clothing.sql
@@ -0,0 +1,20 @@
+INSERT INTO allowed_value (attribute_id,allowed_value)
+VALUES (3,'Male');
+
+INSERT INTO allowed_value (attribute_id,allowed_value)
+VALUES (3,'Female');
+
+INSERT INTO allowed_value (attribute_id,allowed_value)
+VALUES (3,'Any');
+
+INSERT INTO allowed_value (attribute_id,allowed_value)
+VALUES (2,'S');
+
+INSERT INTO allowed_value (attribute_id,allowed_value)
+VALUES (2,'M');
+
+INSERT INTO allowed_value (attribute_id,allowed_value)
+VALUES (2,'L');
+
+INSERT INTO allowed_value (attribute_id,allowed_value)
+VALUES (2,'XL');
