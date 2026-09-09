12bd07f1d5846eb6d0938442eb0a077845a83b7b
V0idKernel621 <m.rezaelahi90@gmail.com>
2026-09-09 16:42:53 +0330

    Create 02_object_types.sql
    made 7 objects

---
+++ sql/02_object_types.sql
@@ -0,0 +1,7 @@
+INSERT INTO object_type (id, name) VALUES (1,'Food');
+INSERT INTO object_type (id, name) VALUES (2,'Clothing');
+INSERT INTO object_type (id, name) VALUES (3,'Appliances');
+INSERT INTO object_type (id, name) VALUES (4,'Entertainment');
+INSERT INTO object_type (id, name) VALUES (5,'Electronics');
+INSERT INTO object_type (id, name) VALUES (6,'Car Parts');
+INSERT INTO object_type (id, name) VALUES (7,'Furniture');
