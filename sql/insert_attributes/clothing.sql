6088696d995cae064b897b76c00967c57c0f5b2e
V0idKernel621 <m.rezaelahi90@gmail.com>
2026-09-09 16:49:37 +0330

    Create clothing.sql
    Attributes for Clothing type object

---
+++ sql/insert_attributes/clothing.sql
@@ -0,0 +1,14 @@
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (1,2,'Color','TEXT');
+
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (2,2,'Size','TEXT');
+
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (3,2,'Gender','TEXT');
+
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (4,2,'Type','TEXT');
+
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (5,2,'Material','TEXT');
