ed4ead5cb80d0d00ea954d9e6a4f7e4a7230f3c5
V0idKernel621 <m.rezaelahi90@gmail.com>
2026-09-11 20:19:21 +0330

    Update 05-Electronics.sql

--- sql/insert_attributes/05-Electronics.sql
+++ sql/insert_attributes/05-Electronics.sql
@@ -1,9 +1,14 @@
 INSERT INTO attribute (id,type_id,name,data_type)
-VALUES (27,5,'Type','TEXT');
-Brand — TEXT
-Type — TEXT
-Model — TEXT
-Color — TEXT
-Storage — NUMBER
-Storage Unit — TEXT
-Warranty — TEXT
+VALUES (27,5,'Brand','TEXT');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (28,5,'Type','TEXT');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (29,5,'Model','TEXT');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (30,5,'Color','TEXT');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (31,5,'Storage','NUMBER');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (32,5,'Storage Unit','TEXT');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (33,5,'Warranty','TEXT');
