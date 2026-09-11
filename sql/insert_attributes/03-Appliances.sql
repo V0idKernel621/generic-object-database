e2cdcbc7d6d2ab1b2df8d51be584374bbedcef94
V0idKernel621 <m.rezaelahi90@gmail.com>
2026-09-11 20:15:07 +0330

    Update 03-Appliances.sql

--- sql/insert_attributes/03-Appliances.sql
+++ sql/insert_attributes/03-Appliances.sql
@@ -1,14 +1,14 @@
 INSERT INTO attribute (id,type_id,name,data_type)
-VALUES (14,1,'Brand','TEXT');
+VALUES (14,3,'Brand','TEXT');
 INSERT INTO attribute (id,type_id,name,data_type)
-VALUES (15,1,'Type','TEXT');
+VALUES (15,3,'Type','TEXT');
 INSERT INTO attribute (id,type_id,name,data_type)
-VALUES (16,1,'Model','TEXT');
+VALUES (16,3,'Model','TEXT');
 INSERT INTO attribute (id,type_id,name,data_type)
-VALUES (17,1,'Power','NUMBER');
+VALUES (17,3,'Power','NUMBER');
 INSERT INTO attribute (id,type_id,name,data_type)
-VALUES (18,1,'Power Unit','TEXT');
+VALUES (18,3,'Power Unit','TEXT');
 INSERT INTO attribute (id,type_id,name,data_type)
-VALUES (19,1,'Color','TEXT');
+VALUES (19,3,'Color','TEXT');
 INSERT INTO attribute (id,type_id,name,data_type)
-VALUES (20,1,'Energy Rating','TEXT');
+VALUES (20,3,'Energy Rating','TEXT');
