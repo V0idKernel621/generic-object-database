ad4985c05d16a92e8c9e3502782311633a4e5b4e
V0idKernel621 <m.rezaelahi90@gmail.com>
2026-09-11 15:49:50 +0330

    Update clothing.sql

--- sql/insert objects/clothing.sql
+++ sql/insert objects/clothing.sql
@@ -1,17 +1,38 @@
 INSERT INTO object (id,type_id,name)
 VALUES (1,2,'Red T-Shirt');
-
 INSERT INTO object_value (object_id, attribute_id, text_value)
 VALUES (1, 1, 'Red');
-
 INSERT INTO object_value (object_id, attribute_id, text_value)
 VALUES (1, 2, 'L');
-
 INSERT INTO object_value (object_id, attribute_id, text_value)
 VALUES (1, 3, 'Any');
-
 INSERT INTO object_value (object_id, attribute_id, text_value)
 VALUES (1, 4, 'Shirt');
-
 INSERT INTO object_value (object_id, attribute_id, text_value)
 VALUES (1, 5, 'Cotton');
+
+INSERT INTO object (id,type_id,name)
+VALUES (2,2,'Silver Dress');
+INSERT INTO object_value (object_id, attribute_id, text_value)
+VALUES (2, 1, 'Silver');
+INSERT INTO object_value (object_id, attribute_id, text_value)
+VALUES (2, 2, 'M');
+INSERT INTO object_value (object_id, attribute_id, text_value)
+VALUES (2, 3, 'Female');
+INSERT INTO object_value (object_id, attribute_id, text_value)
+VALUES (2, 4, 'Dress');
+INSERT INTO object_value (object_id, attribute_id, text_value)
+VALUES (2, 5, 'Silk');
+
+INSERT INTO object (id,type_id,name)
+VALUES (3,2,'Black Leather Jacket');
+INSERT INTO object_value (object_id, attribute_id, text_value)
+VALUES (3, 1, 'Black');
+INSERT INTO object_value (object_id, attribute_id, text_value)
+VALUES (3, 2, 'M');
+INSERT INTO object_value (object_id, attribute_id, text_value)
+VALUES (3, 3, 'Any');
+INSERT INTO object_value (object_id, attribute_id, text_value)
+VALUES (3, 4, 'Jacket');
+INSERT INTO object_value (object_id, attribute_id, text_value)
+VALUES (3, 5, 'Leather');
