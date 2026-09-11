3bd16a407af036c759ba3bb107bf1b58a41e480c
V0idKernel621 <m.rezaelahi90@gmail.com>
2026-09-11 20:01:45 +0330

    Update Food.sql

--- sql/insert_attributes/Food.sql
+++ sql/insert_attributes/Food.sql
@@ -1,1 +1,20 @@
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (7,1,'Brand','TEXT');
 
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (8,1,'Type','TEXT');
+
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (9,1,'Weight','Number');
+
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (10,1,'Unit','TEXT');
+
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (11,1,'Flavor','TEXT');
+
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (12,1,'Expiration Date','Date');
+
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (13,1,'Packaging','TEXT');
