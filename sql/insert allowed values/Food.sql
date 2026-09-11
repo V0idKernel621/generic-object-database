ef239f99af28cab67b41aee081d5e7c8cc69eba5
V0idKernel621 <m.rezaelahi90@gmail.com>
2026-09-11 20:05:20 +0330

    Create Food.sql

---
+++ sql/insert allowed values/Food.sql
@@ -0,0 +1,11 @@
+INSERT INTO allowed_value (attribute_id,allowed_value)
+VALUES (9,'kg');
+
+INSERT INTO allowed_value (attribute_id,allowed_value)
+VALUES (9,'g');
+
+INSERT INTO allowed_value (attribute_id,allowed_value)
+VALUES (9,'ml');
+
+INSERT INTO allowed_value (attribute_id,allowed_value)
+VALUES (9,'L');
