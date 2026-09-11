0daec5be0251334ca8931adfefb0b12012eac101
V0idKernel621 <m.rezaelahi90@gmail.com>
2026-09-11 20:16:12 +0330

    Create 05-Electronics.sql

---
+++ sql/insert_attributes/05-Electronics.sql
@@ -0,0 +1,9 @@
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (27,5,'Type','TEXT');
+Brand — TEXT
+Type — TEXT
+Model — TEXT
+Color — TEXT
+Storage — NUMBER
+Storage Unit — TEXT
+Warranty — TEXT
