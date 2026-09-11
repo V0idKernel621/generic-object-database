f0219a166004407497875b7c10b434249219a9c3
V0idKernel621 <m.rezaelahi90@gmail.com>
2026-09-11 20:22:02 +0330

    Create 06-Furniture.sql

---
+++ sql/insert_attributes/06-Furniture.sql
@@ -0,0 +1,16 @@
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (35,6,'Brand','TEXT');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (36,6,'Type','TEXT');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (37,6,'Material','TEXT');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (38,6,'Color','TEXT');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (39,6,'Width','NUMBER');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (40,6,'Height','NUMBER');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (41,6,'Depth','NUMBER');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (42,6,'Assembly Required','TEXT');
