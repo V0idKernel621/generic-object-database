5ad25095b603a18ec46b08b076b78629a4d37388
V0idKernel621 <m.rezaelahi90@gmail.com>
2026-09-11 20:24:02 +0330

    Update and rename clothing.sql to 02-Clothing.sql

--- sql/insert allowed values/02-Clothing.sql
+++ sql/insert allowed values/02-Clothing.sql
@@ -1,20 +1,14 @@
 INSERT INTO allowed_value (attribute_id,allowed_value)
 VALUES (3,'Male');
-
 INSERT INTO allowed_value (attribute_id,allowed_value)
-VALUES (3,'Female');
-
+VALUES (3,'Female');  
 INSERT INTO allowed_value (attribute_id,allowed_value)
 VALUES (3,'Any');
-
 INSERT INTO allowed_value (attribute_id,allowed_value)
 VALUES (2,'S');
-
 INSERT INTO allowed_value (attribute_id,allowed_value)
 VALUES (2,'M');
-
 INSERT INTO allowed_value (attribute_id,allowed_value)
 VALUES (2,'L');
-
 INSERT INTO allowed_value (attribute_id,allowed_value)
 VALUES (2,'XL');
