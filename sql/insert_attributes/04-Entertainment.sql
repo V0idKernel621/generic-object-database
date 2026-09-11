204b1d6cf15255524478dc1c37519cf04e94c6a3
V0idKernel621 <m.rezaelahi90@gmail.com>
2026-09-11 20:14:40 +0330

    Create 04-Entertainment.sql

---
+++ sql/insert_attributes/04-Entertainment.sql
@@ -0,0 +1,12 @@
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (21,4,'Type','TEXT');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (22,4,'Genre','TEXT');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (23,4,'Creator','TEXT');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (24,4,'Release Year','NUMBER');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (25,4,'Language','TEXT');
+INSERT INTO attribute (id,type_id,name,data_type)
+VALUES (26,4,'Format','TEXT');
