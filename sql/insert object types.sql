3dcdc32b4d27cb118655e847b02c218d43a7fb18
V0idKernel621 <m.rezaelahi90@gmail.com>
2026-09-12 18:16:08 +0330

    Update insert object types.sql

--- sql/insert object types.sql
+++ sql/insert object types.sql
@@ -3,5 +3,5 @@ INSERT INTO object_type (id, name) VALUES (2,'Clothing');
 INSERT INTO object_type (id, name) VALUES (3,'Appliances');
 INSERT INTO object_type (id, name) VALUES (4,'Entertainment');
 INSERT INTO object_type (id, name) VALUES (5,'Electronics');
-INSERT INTO object_type (id, name) VALUES (6,'Car Parts');
-INSERT INTO object_type (id, name) VALUES (7,'Furniture');
+INSERT INTO object_type (id, name) VALUES (6,'Furniture');
+INSERT INTO object_type (id, name) VALUES (7,'Car Parts');
